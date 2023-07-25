0dc0ecebb1cbe10924d303ca94b181422c49cf79
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-07-25 04:37:11 +0400

    Добавьте файлы проекта.

---
+++ Decimal_Square_Root_Newton/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static decimal SquareRoot(long num)
+    {
+        decimal initialGuess = num / 2;
+        for (int i = 0; i < 100; i++)
+        {
+            decimal nextGuess = (initialGuess + num / initialGuess) / 2;
+            if (Math.Abs(nextGuess - initialGuess) < 1e-28M) return nextGuess;
+            initialGuess = nextGuess;
+        }
+        return initialGuess;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(SquareRoot(4503599627370497));
+    }
+}
